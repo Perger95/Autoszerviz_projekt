589543a3da1e4377b5a3f2dec2a86deb1d98fd50
Perger Tamas <perger.tamas95@gmail.com>
2025-04-09 16:29:38 +0200

    Final update

--- app/Http/Controllers/Api/CarController.php
+++ app/Http/Controllers/Api/CarController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Service;
+use App\Models\Car;
 
 class CarController extends Controller
 {
@@ -16,17 +17,19 @@ class CarController extends Controller
 
     public function getCarServices($clientId, $carId)
 {
-    $services = Service::with('car')
-        ->where('client_id', $clientId)
-        ->where('car_id', $carId)
-        ->orderBy('lognumber', 'desc')
-        ->get()
-        ->map(function ($service) {
-            if ($service->event === 'regisztralt' && $service->car) {
-                $service->registered = $service->car->registered;
-            }
-            return $service;
-        });
+    $services = Service::where('client_id', $clientId)
+    ->where('car_id', $carId)
+    ->orderBy('lognumber', 'desc')
+    ->get()
+    ->map(function ($service) {
+        if ($service->event === 'regisztralt') {
+            $car = Car::where('client_id', $service->client_id)
+                      ->where('car_id', $service->car_id)
+                      ->first();
+            $service->registered = $car->registered ?? null;
+        }
+        return $service;
+    });
 
     return response()->json($services);
 }
